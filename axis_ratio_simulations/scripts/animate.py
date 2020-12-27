4525b7a0fc258721a3ea66fa70396c5536418e0c
Dane Cross <dane.cross2016@gmail.com>
2020-12-27 12:10:53 -0500

    add axes to animation

--- axis_ratio_simulations/scripts/animate.py
+++ axis_ratio_simulations/scripts/animate.py
@@ -24,34 +24,8 @@ if len(sys.argv)<2:
     print("ERROR: must input which omega to animate")
     exit()
 
-filepath="../data/outputs_"+sys.argv[1]+"/conf3s"
-dirs = os.listdir(filepath)
-for i in range(len(dirs)):
-    try:
-        dirs[i] = int(dirs[i][7:])
-    except:
-        dirs[i] = 0
-maxnum = max(dirs)
 
-x = None
-i = 0 ; T = []
-while i < maxnum:
-    file_conf3 = filepath+"/conf.3_"+str(i)
-    i+=1
-    if not os.path.exists(file_conf3):
-        continue
-
-    t, _, _, p, _ = nb.read_conf3(file_conf3)
-    if x is None:
-        x = [p[:,0]]
-        y = [p[:,1]]
-        z = [p[:,2]]
-    else:
-        x += [p[:,0]]
-        y += [p[:,1]]
-        z += [p[:,2]]
-        if i%100==0:
-            print(len(y))
+#extract axes
 lim = 15
 filepath = "../data/pickled_data/"+sys.argv[1]+"/axes.pkl"
 axes = pickle.load(open(filepath, "rb")) 
@@ -66,6 +40,11 @@ z_axes_y = [[-lim*vecs[2][1], lim*vecs[2][1]] for vecs in axes]
 x_axes_z = [[-lim*vecs[0][2], lim*vecs[0][2]] for vecs in axes]
 y_axes_z = [[-lim*vecs[1][2], lim*vecs[1][2]] for vecs in axes]
 z_axes_z = [[-lim*vecs[2][2], lim*vecs[2][2]] for vecs in axes]
+
+
+#extract positions
+filepath = "../data/pickled_data/"+sys.argv[1]+"/positions.pkl"
+p = pickle.load(open(filepath, "rb"))
 
 fig = plt.figure() 
 ax = plt.axes(xlim=(-lim, lim), ylim=(-lim, lim), projection='3d')
@@ -84,8 +63,13 @@ def init():
     return bodies, x_axis, y_axis, z_axis,
 
 def animate(i):
-    bodies.set_data(x[i%len(x)], y[i%len(y)])
-    bodies.set_3d_properties(z[i%len(z)])
+
+    x = [p[i][j][0] for j in range(len(p[i]))]
+    y = [p[i][j][1] for j in range(len(p[i]))]
+    z = [p[i][j][2] for j in range(len(p[i]))]
+
+    bodies.set_data(x, y)
+    bodies.set_3d_properties(z)
 
     x_axis.set_data(x_axes_x[i%len(x)], x_axes_y[i%len(x)]) 
     x_axis.set_3d_properties(x_axes_z[i%len(x)])
@@ -102,7 +86,6 @@ anim = animation.FuncAnimation(fig, animate, init_func=init,
                                frames=500, interval=1000, blit=True)
 
 plt.show()
-
 
 
 
