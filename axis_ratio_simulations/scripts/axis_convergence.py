d302779d37ab232eb6303c4308a4faee46c50992
Dane Cross <dane.cross2016@gmail.com>
2020-12-29 12:57:51 -0500

    bug fixes in extracting ratios

--- axis_ratio_simulations/scripts/axis_convergence.py
+++ axis_ratio_simulations/scripts/axis_convergence.py
@@ -3,15 +3,25 @@ import numpy as np
 from scipy.stats import norm
 
 def q_calc(x, y, z, M):
-        ba = np.sqrt(M[0]/M[2])
+        ac = np.sqrt(M[0]/M[2])
         bc = np.sqrt(M[1]/M[2])
-        return np.array([np.sqrt(x[i]**2 + (y[i]/ba)**2 + (z[i]/bc)**2) for i in range(len(z))])
+        return np.array([np.sqrt(x[i]**2 + (y[i]/ac)**2 + (z[i]/bc)**2) for i in range(len(z))])
 
 def M_calc(p, q):
 
-        M = [[np.sum(p[:,i]*p[:,j]/q**2) for j in range(3)] for i in range(3)]
-        eigenvalues, eigenvectors = np.linalg.eig(M)
+        M = np.zeros((3, 3))
 
+        M[0,0] = np.sum(p[:,0]*p[:,0]/q**2)
+        M[0,1] = np.sum(p[:,0]*p[:,1]/q**2)
+        M[0,2] = np.sum(p[:,0]*p[:,2]/q**2)
+        M[1,1] = np.sum(p[:,1]*p[:,1]/q**2)
+        M[1,2] = np.sum(p[:,1]*p[:,2]/q**2)
+        M[2,2] = np.sum(p[:,2]*p[:,2]/q**2)
+
+        M[1,0]=M[0,1] ; M[2,0]=M[0,2] ; M[2,1]=M[1,2]
+
+        eigenvalues, eigenvectors = np.linalg.eig(M)
+        eigenvectors = [x for _,x in sorted(zip(eigenvalues,eigenvectors))]
         eigenvalues.sort()
 
         return eigenvalues, eigenvectors
--- axis_ratio_simulations/scripts/ratios_graphing.py
+++ axis_ratio_simulations/scripts/ratios_graphing.py
@@ -32,7 +32,7 @@ T = [pickle.load(open("../data/pickled_data/"+ filenames[i] +"/T.pkl", "rb")) fo
 
 # half-mass relaxation data for the x-axis
 cluster_data = [pickle.load(open("../data/pickled_data/"+ filenames[i] +"/cluster_data.pkl", "rb")) for i in range(len(filenames))]
-N = [cluster_data[i][0] for i in range(len(cluster_data))]
+N = [50000 for i in range(3)]
 r_h = [cluster_data[i][1] for i in range(len(cluster_data))]
 
 fig_full, ax_full = plt.subplots(3, 1, sharex=True, sharey=True)
@@ -41,18 +41,20 @@ fig_full.set_size_inches(20, 20)
 names = ["0.3" , "0.6", "1.2"] 
 for i in range(len(names)):
 
-    hmrt = (0.138)*N[i]*r_h[i]**(3/2)/np.log10(0.11*N[i])
+    hmrt = (0.138)*N[i]*(r_h[i]**(3/2))/np.log(0.11*N[i])
     x = [T[i][j]/hmrt for j in range(len(T[i]))]
+    print(hmrt, r_h[i])
 
     a = np.array([M[i][j][0] for j in range(len(M[i]))])
     b = np.array([M[i][j][1] for j in range(len(M[i]))])
     c = np.array([M[i][j][2] for j in range(len(M[i]))])
 
-    print(len(x), len(a))
-    
+    ac = np.sqrt(a/c)
+    bc = np.sqrt(b/c)
+
     fig, ax = plt.subplots(4, 1, sharex=True, sharey=False)
-    ax[0].plot(x, a/c, label="b/a ratio", color=colors[0]) ; ax[0].set_title("ratios")
-    ax[0].plot(x, b/c, label="c/a ratio", color=colors[1]) ; ax[0].legend()
+    ax[0].plot(x, ac, label="b/a ratio", color=colors[0]) ; ax[0].set_title("ratios")
+    ax[0].plot(x, bc, label="c/a ratio", color=colors[1]) ; ax[0].legend()
     ax[0].set_ylim((0, 1.3))
 
     ax[1].plot(x, a, color=colors[0]) ; ax[1].set_title("a (x-axis)")
@@ -66,9 +68,6 @@ for i in range(len(names)):
     ax_full[i].plot(x, a/c, label="b/a ratio", color=colors[0])
     ax_full[i].plot(x, b/c, label="c/a ratio", color=colors[1])
 
-    #orig = ax_full[i].get_xticks()
-    #new = [int(float(orig[j])/hmrt[i]) for j in range(len(orig))]
-    #ax_full[i].set_xticks(new)
 
 
 ax_full[2].legend(bbox_to_anchor=(0.5, -0.6), loc='lower center')
