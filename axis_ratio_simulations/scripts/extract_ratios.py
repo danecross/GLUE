3c7a0165d02a7a439a2a9f2bfd96f445e1af3554
Dane Cross <dncross@h2.carbonate.uits.iu.edu>
2021-02-04 09:38:17 -0500

    delete chunk code

--- axis_ratio_simulations/scripts/extract_ratios.py
+++ axis_ratio_simulations/scripts/extract_ratios.py
@@ -34,63 +34,16 @@ maxnum = max(dirs)
 t, _, _, p, _ = nb.read_conf3(filepath+"/conf.3_0")
 HALF_MASS_RADIUS = np.median([np.sqrt(p[i][0]**2 + p[i][1]**2 + p[i][2]**2) for i in range(len(p))])
 
-if len(sys.argv) > 2:
-    chunk = int(sys.argv[2])
-    of = int(sys.argv[3])
-    
-    if chunk > of:
-        print("ERROR: sys.argv[2] must be greater than sys.argv[3]")
-        exit()
-
-    chunk_len = maxnum//of
-    i = chunk_len*(chunk)
-    maxnum = chunk_len*(chunk+1)
-else:
-    i = 0
-
 print("Starting extraction:")
 
 M, T = ac.extract_ratios(filepath, i, maxnum, HALF_MASS_RADIUS, lower_shell=.1, upper_shell=.2)
-
+M = np.array(M)
+T = np.array(T)
 
 newdir = "../data/pickled_data/"+sys.argv[1]
-
-if len(sys.argv)==2 or not os.path.exists(newdir):
-    if os.path.exists(newdir):
-        shutil.rmtree(newdir)
-    os.mkdir(newdir)
-
-    M = np.array(M)
-    T = np.array(T)
-
-else:
-    M_old = pickle.load(open(os.path.join(newdir, "M.pkl"), "rb"))
-    T_old = pickle.load(open(os.path.join(newdir, "T.pkl"), "rb"))
-
-    start = chunk_len*(chunk)
-    i = 0 ; j = 0
-    while i < start:
-        file_conf3 = filepath+"/conf.3_"+str(i)
-        if os.path.exists(file_conf3):
-            j+=1
-        i+=1
-    start_index = j
-    end_index = j+len(M)
-
-    if start_index > len(M_old):
-        M_old += [0]*(start_index-len(M_old))
-        T_old += [0]*(start_index-len(M_old))
-    if end_index > len(M_old):
-        M_old += [0]*(end_index-len(M_old))
-        T_old += [0]*(end_index-len(M_old))
-    
-    for i in range(start_index, end_index):
-        M_old[i] = M[i-start_index]
-        T_old[i] = T[i-start_index]
-
-    M = np.array(M_old)
-    T = np.array(T_old)
-
+if os.path.exists(newdir):
+    shutil.rmtree(newdir)
+os.mkdir(newdir)
 
 pickle.dump(M, open(os.path.join(newdir, "M.pkl"), "wb"))
 pickle.dump(T, open(os.path.join(newdir, "T.pkl"), "wb"))
