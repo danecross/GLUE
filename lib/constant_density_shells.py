b79b3c012582acff68ff98928fc9d190bc27179f
Dane Cross <dane.cross2016@gmail.com>
2021-05-25 13:45:49 -0400

    eigen-analysis of constant density shells

---
+++ lib/constant_density_shells.py
@@ -0,0 +1,42 @@
+from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit, minimize
+
+import ellipse_functions as ef
+import axis_convergence as ac
+import numpy as np
+
+# input: 2D data, lower ellipse cut, spacing between the semi-major axes
+# output: the semi-minor axis which determines the upper cut. This semi-minor
+#         axis creates a cut that is as close to a constant density profile as 
+#         possible
+def shell_fit2D(p, lower_ellipse, a_diff, max_iter=25, converge_radius=1e-4):
+    # TODO
+
+# returns the expected eigenvalues for a specified shape with constant density
+def constant_density_eigs(lower_ellipse, upper_ellipse, num_stars):
+    
+    const_density_shape = create_const_density_distribution(lower_ellipse, upper_ellipse, num_stars)
+    M, evecs = ac.iterate_2D(const_density_shape)
+
+    return M, evecs
+
+def create_const_density_distribution(lower_ellipse, upper_ellipse, num_stars):
+    #area to be covered
+    A = np.pi * (upper_ellipse[0] * upper_ellipse[1] - lower_ellipse[0] * lower_ellipse[1])
+    rho = num_stars/A #<-- number density
+
+    box = create_const_box(2*upper_ellipse[0], 2*upper_ellipse[1], rho)
+
+    return ef.ellipse_cut(box, lower_ellipse, upper_ellipse)
+
+def create_const_box(w, h, rho):
+
+    x = np.linspace(-w/2, w/2, int(w*np.sqrt(rho)))
+    y = np.linspace(-h/2, h/2, int(h*np.sqrt(rho)))
+    
+    #return cartesian coordinates of x and y
+    return np.transpose([np.tile(x, len(y)), np.repeat(y, len(x))])
+
+    
+
+
---
+++ scripts/algorithm_building/test_cds_helper_funcs.py
@@ -0,0 +1,115 @@
+
+import constant_density_shells as cds
+from plotting_aid import plot_axes_from_evecs_2D
+
+import sys
+from matplotlib import pyplot as plt
+plt.style.use('../../../../default_stylesheet.mplstyle')
+fig = plt.gcf() ; fig.set_size_inches(50,25)
+
+
+img_path = "cds_test_pngs/helper_func_pngs/"
+
+def plot_all(pos, vecs, M, name, a):
+    plt.gcf().set_size_inches(20,20)
+    plt.plot(pos[:,0], pos[:,1], '.')
+
+    b = a*(M[1]/M[0])**(1/2)
+    axes = plot_axes_from_evecs_2D(vecs, [a, b])
+    for ax in axes:
+        plt.plot(ax[0], ax[1], color="black")
+
+    plt.gca().set_aspect('equal', 'box')
+    plt.savefig(name)
+
+#########################
+###  create test case ###
+#########################
+
+lower_ellipse = (10, 5)
+upper_ellipse = (30, 25)
+num_stars = int(sys.argv[1])
+
+
+################################
+##  test create_const_box  #####
+################################
+
+height = 20 ; width = 10 ; density = 100
+
+box = cds.create_const_box(width, height, density)
+plt.plot(box[:,0], box[:,1], '.')
+plt.gca().set_aspect('equal', 'box')
+plt.savefig(img_path+"const_box_test_" + str(density) + ".png")
+plt.cla()
+
+#############################################
+##  test create_const_density_distribution ##
+#############################################
+
+dist = cds.create_const_density_distribution(lower_ellipse, upper_ellipse, num_stars)
+plt.plot(dist[:,0], dist[:,1], '.')
+plt.gca().set_aspect('equal', 'box')
+plt.savefig(img_path+"const_cutout_test_" + str(num_stars) + ".png")
+plt.cla()
+
+################################################
+## test constant_density_eigs (basic ellipse) ##
+################################################
+
+box = cds.create_const_density_distribution((1e-6,1e-6), upper_ellipse, num_stars)
+M, evecs = cds.constant_density_eigs((1e-6,1e-6), upper_ellipse, num_stars)
+plot_all(box, evecs, M, img_path+"basic_cde_" + str(num_stars) + ".png", upper_ellipse[0])
+plt.cla()
+
+
+#############################################
+## test constant_density_eigs (thin shell) ##
+#############################################
+
+thickness = 1
+
+box = cds.create_const_density_distribution((upper_ellipse[0]-thickness*2, upper_ellipse[1]-thickness), upper_ellipse, num_stars)
+M, evecs = cds.constant_density_eigs((1e-6,1e-6), upper_ellipse, num_stars)
+plot_all(box, evecs, M, img_path+"cde_thin_shell_" + str(num_stars) + ".png", upper_ellipse[0])
+plt.cla()
+
+
+#################################################
+## test constant_density_eigs (thin-ish shell) ##
+#################################################
+
+box = cds.create_const_density_distribution(lower_ellipse, upper_ellipse, num_stars)
+M, evecs = cds.constant_density_eigs((1e-6,1e-6), upper_ellipse, num_stars)
+plot_all(box, evecs, M, img_path+"cde_shell_" + str(num_stars) + ".png", upper_ellipse[0])
+plt.cla()
+
+
+#################################################
+## test constant_density_eigs (shell series)   ##
+#################################################
+
+lower_ellipses = [(15, i) for i in range(2, 21, 3)]
+Ms = []
+for lower_ellipse in lower_ellipses:
+    box = cds.create_const_density_distribution(lower_ellipse, upper_ellipse, num_stars)
+    M, evecs = cds.constant_density_eigs(lower_ellipse, upper_ellipse, num_stars)
+    plot_all(box, evecs, M, img_path+"series_" + str(num_stars) + "/cde_shell_" + str(lower_ellipse[1]) + ".png", upper_ellipse[0])
+    Ms += [M[0]/M[1]]
+    plt.cla()
+
+plt.clf()
+
+plt.style.use('../../../../default_stylesheet.mplstyle')
+fig = plt.gcf() ; fig.set_size_inches(50,25)
+
+plt.plot([i for i in range(2, 21, 3)], Ms)
+plt.title("axis ratios as a function of semi-minor axis")
+plt.savefig(img_path+"series_ar_" + str(num_stars) + ".png")
+
+
+
+
+
+
+
